daea5b2f999875073b83dab1598cc8332a0861a6
LUKAS VRBA <luky.vrba1@seznam.cz>
2021-03-22 21:59:42 +0100

    progress

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set stataic folder
+app.use(express.static(path.join(__dirname, 'livechat')));
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...');
+})
+
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
