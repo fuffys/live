beb3901f71b44fec851a3213c6a4979788d8b2d9
LUKAS VRBA <luky.vrba1@seznam.cz>
2021-03-23 20:31:32 +0100

    IFoundError

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
-
+const {
+    userJoin, 
+    getCurrentUser, 
+    userLeave, 
+    getRoomUsers
+    } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,18 +24,24 @@ io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
         const user = userJoin(socket.id, username, room );
 
-
         socket.join(user.room);
-
 
     //Welcome current user
     socket.emit('message', formatMessage(botName,  'Vítejte v Chatu!'));
 
     //Broadcast when user connects
     socket.broadcast
-    .to(user.room)
-    .emit('message', formatMessage(botName,  `${user.username} se připojil`));
+        .to(user.room)
+        .emit(
+            'message', 
+            formatMessage(botName,  `${user.username} se připojil`)
+            );
 
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
     });
 
     // Listen for chatMessage
@@ -46,12 +56,17 @@ io.on('connection', socket => {
         const user = userLeave(socket.id);
 
         if(user) {
-            io.to(user.room).emit('message', formatMessage(botName,  `${user.username} se odpojil`));
-        }
-        
-    });
-
-
+            io.to(user.room)
+            .emit('message', 
+            formatMessage(botName, `${user.username} se odpojil`)
+            );
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
